600c3e0d57f44d89aa4e23f430e0350392ed7c7f
Bethelhem Jankebed <bettyj4565@gmail.com>
2025-09-21 18:25:20 +0300

    corrected

---
+++ script.js
@@ -0,0 +1,32 @@
+// Global inventory array
+let inventory = [];
+
+// 1. Add Product
+function addProduct(id, name, price, quantity) {
+  // TODO: bethelhem jankebed
+}
+
+// 2. Remove Product
+function removeProduct(id) {
+  // TODO: queen odede
+}
+
+// 3. Update Stock
+function updateStock(id, newQuantity) {
+  // TODO: senayt
+}
+
+// 4. Generate Report
+function generateReport() {
+  // TODO: starmomo
+}
+
+//  SAMPLE USAGE / TESTING 
+addProduct("P001", "Notebook", 2, 20);
+addProduct("P002", "Pen", 1, 50);
+addProduct("P003", "Stapler", 7, 5);
+
+updateStock("P001", 25);
+removeProduct("P002");
+
+generateReport();
